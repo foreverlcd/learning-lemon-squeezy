2f294b6f8a0d12a8297851c3fbc446619a195045
foreverlcd <215422@unsaac.edu.pe>
2026-01-29 08:55:56 -0500

    fix: solving problems with prisma version

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,21 @@
-import { PrismaClient } from '@prisma/client';
+
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: InstanceType<typeof PrismaClient> | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: 'file:./dev.db',
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: 'file:./dev.db',
+      },
     },
-  },
-});
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
